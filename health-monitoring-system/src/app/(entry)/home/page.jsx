c38ef118cd4b87f903a6317e07d2cab92ec24be3
Yusuf <y.abdulwasiu@gmail.com>
2024-10-31 14:59:19 +0100

    feat: :sparkles: landing page

---
+++ health-monitoring-system/src/app/(entry)/home/page.jsx
@@ -0,0 +1,12 @@
+
+import Home from "@/components/HomeComponents/Home/Home";   
+
+function HelathMontioringSystem() {
+    return (
+        <>
+            <Home/>
+        </>
+    );
+}
+
+export default HelathMontioringSystem;
